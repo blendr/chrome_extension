eac45b833217f3834143502f187c41dc33036f7d
Connie Qi <connieqi@umich.edu>
2015-03-07 23:33:45 -0500

    using KartikTalwar's gmail.js, getting recently composed draft ids

---
+++ js/content.js
@@ -0,0 +1,11 @@
+var j = document.createElement('script');
+j.src = chrome.extension.getURL('/js/lib/jquery.min.js');
+(document.head || document.documentElement).appendChild(j);
+
+var g = document.createElement('script');
+g.src = chrome.extension.getURL('/js/lib/gmail.js');
+(document.head || document.documentElement).appendChild(g);
+
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('/js/main.js');
+(document.head || document.documentElement).appendChild(s);
--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 // Add cahoots link to elt. Elt should be the button toolbar at the bottom of a
 // draft popup
 var addLink = function(elt) {
-    // console.log("number of links is " + $(elt).children('.cahoots-button').length);
+    console.log("number of links is " + $(elt).children('.cahoots-button').length);
 
     if (elt !== null && $(elt).children('.cahoots-button').length == 0) {
-        // console.log("appending");
+        console.log("appending");
+        // $(elt).append(
+        //     '<div class="cahoots-button"><a href=\"http://cahoots.email\">cahoots!</a></div>'
+        // );
+
         $(elt).append(
-            '<div class="cahoots-button"><a href=\"http://cahoots.email\">cahoots!</a></div>'
+            '<input type="button" class="cahoots-button" value="cahoots">'
         );
+
+        $(elt).on('click', '.cahoots-button', function() {
+            var gmail = new Gmail();
+            console.log(gmail.get.compose_ids());
+        });
     }
 };
 
@@ -18,24 +27,44 @@ function addLinkToElts() {
 
     if(elements.is(':visible')) {
         elements.each(function(i, elt) {
-            // console.log("calling addLink for elt " + i);
+            console.log("calling addLink for elt " + i);
             addLink(elt);
         })
     }
 };
 
-$(document).ready(function() {
-    // If compose popups exist on load, append cahoots link
-    var pathname = window.location.href;
-    if (pathname.indexOf('compose') > 0){
-        addLinkToElts();
-    }
+var gmail;
 
-    // Add an event listener to add cahoot link to new draft popups.
-    // XXX: There's a race condition here and we almost always miss the latest
-    // draft
-    var composeButton = $(".T-I.J-J5-Ji.T-I-KE.L3:contains('COMPOSE')").get(0);
-    composeButton.addEventListener('click', function() {
-        addLinkToElts();
-    }, false);
-});
+function refresh(f) {
+  if( (/in/.test(document.readyState)) || (undefined === Gmail) || (undefined === $)) {
+    setTimeout('refresh(' + f + ')', 10);
+  } else {
+    f();
+  }
+}
+
+var main = function(){
+    // NOTE: Always use the latest version of gmail.js from
+    // https://github.com/KartikTalwar/gmail.js
+    gmail = new Gmail();
+    console.log('Hello from cahoots,', gmail.get.user_email());
+
+
+    $(document).ready(function() {
+        // If compose popups exist on load, append cahoots link
+        var pathname = window.location.href;
+        if (pathname.indexOf('compose') > 0){
+            addLinkToElts();
+        }
+
+        // Add an event listener to add cahoot link to new draft popups.
+        // XXX: There's a race condition here and we almost always miss the latest
+        // draft
+        var composeButton = $(".T-I.J-J5-Ji.T-I-KE.L3:contains('COMPOSE')").get(0);
+        composeButton.addEventListener('click', function() {
+            addLinkToElts();
+        }, false);
+    });
+}
+
+refresh(main);
